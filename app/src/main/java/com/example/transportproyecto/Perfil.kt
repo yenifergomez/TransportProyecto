e1c5d11f4f599acb49fa66fb2fb4e4742dc566a9
Yenifer <gomezpapamijayenifer@gmail.com>
2023-11-28 13:43:15 -0500

    editar perfil

--- app/src/main/java/com/example/transportproyecto/Perfil.kt
+++ app/src/main/java/com/example/transportproyecto/Perfil.kt
@@ -1,29 +1,61 @@
 package com.example.transportproyecto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.transportproyecto.databinding.ActivityMainBinding
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.transportproyecto.client.ApiClient
 import com.example.transportproyecto.databinding.ActivityPerfilBinding
+import com.example.transportproyecto.model.response.User
+import com.example.transportproyecto.service.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Perfil : AppCompatActivity() {
 
     private lateinit var binding: ActivityPerfilBinding
+    private lateinit var currentUser: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
-
         binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        clickListener()
 
-        binding.editarPerfil.setOnClickListener {
+        // Obtén la instancia de ApiService
+        val apiService = ApiClient.getApiService()
+
+        // Ejemplo de llamada para obtener el perfil del usuario actual
+        val userId = "123" // Reemplaza con la lógica para obtener el ID del usuario actual
+        getUserProfile(apiService, userId)
+
+        /*binding.editarPerfil.setOnClickListener {
             startActivity(Intent(this@Perfil, Editar_Perfil::class.java))
-        }
+        }*/
     }
 
-    private fun clickListener() {
-        binding.editarPerfil.setOnClickListener {
-        }
+    private fun getUserProfile(apiService: ApiService, userId: String) {
+        val call = apiService.getUserProfile(userId)
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    currentUser = response.body()!!
+                    updateUIWithUserData(currentUser)
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                // Maneja la falla en la solicitud
+            }
+        })
+    }
+
+    private fun updateUIWithUserData(user: User) {
+        // Actualiza la interfaz de usuario con los datos del perfil obtenidos
+        val nameTextView: TextView = findViewById(R.id.name)
+        val emailTextView: TextView = findViewById(R.id.email)
+
+        nameTextView.text = user.usuario
+        emailTextView.text = user.email
     }
 }
--- app/src/main/java/com/example/transportproyecto/service/ApiService.kt
+++ app/src/main/java/com/example/transportproyecto/service/ApiService.kt
@@ -4,9 +4,13 @@ import com.example.transportproyecto.model.request.LoginRequest
 import com.example.transportproyecto.model.request.RegisterRequest
 import com.example.transportproyecto.model.response.LoginResponse
 import com.example.transportproyecto.model.response.RegisterResponse
+import com.example.transportproyecto.model.response.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -15,6 +19,15 @@ interface ApiService {
 
     @POST("/api/register")
     fun registerUser(@Body registerRequest: RegisterRequest): Call<RegisterResponse>
+
+    @GET("api/delete/")
+    fun deleteUser(): Call<User>
+
+    @GET("api/users/{userId}")
+    fun getUserProfile(@Path("userId") userId: String): Call<User>
+
+        @PUT("/users/{userId}")
+        fun updateUserProfile(@Path("userId") userId: String, @Body user: User): Call<User>
 
 }
 
